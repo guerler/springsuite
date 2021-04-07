25f981c099098f5df4326c08aa5d41542362090e
guerler <aysam.guerler@gmail.com>
2021-04-07 13:13:47 -0400

    Improve handling of binary files in dbkit

--- src/dbkit/dbkit_package/DBKit.py
+++ src/dbkit/dbkit_package/DBKit.py
@@ -47,8 +47,8 @@ def writeEntry(identifier, fileName, outputIndex, outputDatabase):
     if entrySize > 0:
         outputIndexFile = open(outputIndex, "a+")
         outputIndexFile.write("%s\t%s\t%s\n" % (identifier, currentSize, entrySize))
-        tempFile = open(fileName, "r")
-        databaseFile = open(outputDatabase, "a+")
+        tempFile = open(fileName, "rb")
+        databaseFile = open(outputDatabase, "ab+")
         databaseFile.write(tempFile.read())
         databaseFile.close()
         tempFile.close()
